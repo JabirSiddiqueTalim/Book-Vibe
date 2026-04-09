ab0eb9afd30541b9ad3bc3705eb7d3b036eb2d38
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-04-09 21:14:22 +0600

    Polishing the product card and adding React Icons

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { FaStar } from "react-icons/fa";
 
 const Book = ({singleData}) => {
   // console.log(singleData)
-  const {bookName,author,image}=singleData;
+  const {bookName,author,image,rating,publisher}=singleData;
   console.log(singleData)
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-    <figure>
-      <img
+    <div className="card bg-base-100 w-96 shadow-sm p-10 ">
+    <figure className='bg-gray-100 p-10 r-30'>
+      <img className='h-60'
         src={image}
         alt="Shoes" />
     </figure>
     <div className="card-body">
       <h2 className="card-title">
-        Card Title
+        {bookName}
         <div className="badge badge-secondary">NEW</div>
       </h2>
-      <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+      <p>Review : {publisher}</p>
       <div className="card-actions justify-end">
-        <div className="badge badge-outline">Fashion</div>
-        <div className="badge badge-outline">Products</div>
+        <div className="badge badge-outline">{author}</div>
+        <div className="badge badge-outline">{rating} <FaStar /></div>
       </div>
     </div>
   </div>
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -5,9 +5,9 @@ import Book from '../Book/Book';
 const Books = ({data}) => {
   return (
     <div>
-      <h2 className='text-3xl text-center p-4'>Books</h2>
+      <h2 className='text-3xl text-blue-950 text-center p-4'>Books</h2>
       <Suspense fallback={<p>Loading</p>}>
-      <div className='grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-20 '>
+      <div className='grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-10 '>
       {
         
         data.map(singleData=> <Book key={singleData.bookId} singleData={singleData}></Book>)
